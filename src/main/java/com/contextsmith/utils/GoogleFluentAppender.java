872943f28d7f29e02c8ea5006db6580aadb52785
beders <beders@yahoo.com>
2017-05-05 13:21:14 -0700

    fixed generics inconsistencies, reformatted code

--- src/main/java/com/contextsmith/utils/GoogleFluentAppender.java
+++ src/main/java/com/contextsmith/utils/GoogleFluentAppender.java
@@ -22,86 +22,86 @@ import java.util.Map;
  * Created by beders on 5/4/17.
  */
 
-@Plugin(name="GoogleFluentAppender", category="Core", elementType="appender", printObject=true)
+@Plugin(name = "GoogleFluentAppender", category = "Core", elementType = "appender", printObject = true)
 public class GoogleFluentAppender extends AbstractAppender {
-	private final HashMap<String,String> serviceContextData;
-	private FluentLogger fluentLogger;
-	private String label;
+    private final HashMap<String, String> serviceContextData;
+    private FluentLogger fluentLogger;
+    private String label;
 
     protected GoogleFluentAppender(String name, Filter filter, Layout<? extends Serializable> layout, boolean ignoreExceptions,
-								String tag, String label,
-								String service) {
-		super(name, filter, layout, ignoreExceptions);
-		fluentLogger = FluentLogger.getLogger(tag);
-		serviceContextData = new HashMap<>();
+                                   String tag, String label,
+                                   String service) {
+        super(name, filter, layout, ignoreExceptions);
+        fluentLogger = FluentLogger.getLogger(tag);
+        serviceContextData = new HashMap<>();
         serviceContextData.put("service", service);
         this.label = label == null ? "" : label;
     }
 
-	@Override
-	public void stop() {
-		super.stop();
-		fluentLogger.close();
-	}
+    @Override
+    public void stop() {
+        super.stop();
+        fluentLogger.close();
+    }
 
-	@Override
-	public void append(LogEvent event) {
-		Map<String, Object> message = new HashMap<>();
+    @Override
+    public void append(LogEvent event) {
+        Map<String, Object> message = new HashMap<>();
 
-		//message.put("thread", event.getThreadName());
-		if (event.getThrown() != null) {
-			StringWriter exceptionWriter = new StringWriter();
-			//exceptionWriter.append(event.getMessage().getFormat());
-			event.getThrown().printStackTrace(new PrintWriter(exceptionWriter));
-			message.put("message", exceptionWriter.toString());
-		} else {
-			message.put("message", "[" + event.getThreadName() + "] " + event.getMessage().getFormattedMessage());
-			message.put("context", context(event));
-		}
-		message.put("serviceContext", serviceContextData);
-		fluentLogger.log(label, message, event.getTimeMillis() / 1000); // thanks for not documenting that Fluent lib
-	}
+        //message.put("thread", event.getThreadName());
+        if (event.getThrown() != null) {
+            StringWriter exceptionWriter = new StringWriter();
+            //exceptionWriter.append(event.getMessage().getFormat());
+            event.getThrown().printStackTrace(new PrintWriter(exceptionWriter));
+            message.put("message", exceptionWriter.toString());
+        } else {
+            message.put("message", "[" + event.getThreadName() + "] " + event.getMessage().getFormattedMessage());
+            message.put("context", context(event));
+        }
+        message.put("serviceContext", serviceContextData);
+        fluentLogger.log(label, message, event.getTimeMillis() / 1000); // thanks for not documenting that Fluent lib
+    }
 
-	private Object context(LogEvent event) {
-		Map<String, Object> context = new HashMap<>();
-		context.put("reportLocation", location(event));
-		return context;
-	}
+    private Object context(LogEvent event) {
+        Map<String, Object> context = new HashMap<>();
+        context.put("reportLocation", location(event));
+        return context;
+    }
 
-	private Object location(LogEvent event) {
-		StackTraceElement source = event.getSource();
-		Map<String, Object> reportLocation = new HashMap<>();
-		reportLocation.put("filePath", source.getFileName());
-		reportLocation.put("lineNumber", source.getLineNumber());
-		reportLocation.put("functionName", source.getMethodName());
-		return reportLocation;
-	}
+    private Object location(LogEvent event) {
+        StackTraceElement source = event.getSource();
+        Map<String, Object> reportLocation = new HashMap<>();
+        reportLocation.put("filePath", source.getFileName());
+        reportLocation.put("lineNumber", source.getLineNumber());
+        reportLocation.put("functionName", source.getMethodName());
+        return reportLocation;
+    }
 
-	@PluginFactory
+    @PluginFactory
     public static GoogleFluentAppender createAppender(
             @PluginAttribute("name") String name,
             @PluginElement("Layout") Layout<? extends Serializable> layout,
             @PluginElement("Filter") final Filter filter,
             @PluginAttribute("tag") String tag,
-			@PluginAttribute("label") String label,
-			@PluginAttribute("service") String service) {
+            @PluginAttribute("label") String label,
+            @PluginAttribute("service") String service) {
         if (name == null) {
             LOGGER.error("No name provided for GoogleFluentAppender");
-			return null;
+            return null;
         }
         if (layout == null) {
             layout = PatternLayout.createDefaultLayout();
         }
         if (tag == null || tag.isEmpty()) {
-			tag = "fluent";
-		}
-		if (service == null || service.isEmpty()) {
-        	service = "app";
-		}
-		if (label == null) {
-        	label = "";
-		}
+            tag = "fluent";
+        }
+        if (service == null || service.isEmpty()) {
+            service = "app";
+        }
+        if (label == null) {
+            label = "";
+        }
 
-		return new GoogleFluentAppender(name, filter, layout, false, tag, label, service);
+        return new GoogleFluentAppender(name, filter, layout, false, tag, label, service);
     }
 }
